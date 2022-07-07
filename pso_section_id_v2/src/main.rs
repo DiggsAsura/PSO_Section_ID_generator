0497c7417a4a0cf6bf0c0101e6cc9ef296ad999d
DiggsAsura <kenneth.bjerke@gmail.com>
2022-07-07 11:03:39 +0100

    Some testings only

--- pso_section_id_v2/src/main.rs
+++ pso_section_id_v2/src/main.rs
@@ -1,3 +1,10 @@
+use rand::seq::SliceRandom;
+
+// quick test to see if i can add stuff outside funcitons
+//let my_lst = ["item1", "item2", "item3"];
+
 fn main() {
-    println!("Hello, world!");
+    let my_lst = ["item1", "item2", "item3"];
+
+    println!("{:?}", my_lst.choose(&mut rand::thread_rng()));
 }
